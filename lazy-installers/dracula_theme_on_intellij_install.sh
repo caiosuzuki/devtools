30292548b387fb98761a9d39593b892dc0e4c5f8
caiosuzuki <cpolidoro@gmail.com>
2019-09-25 17:55:32 -0400

    adding dracula theme installer

---
+++ lazy-installers/dracula_theme_on_intellij_install.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/dracula/jetbrains.git $HOME/jetbrains &&
+	mkdir $HOME/.IntelliJIdea2019.2/config/colors &&
+	cp $HOME/jetbrains/Dracula.icls $HOME/.IntelliJIdea2019.2/config/colors/
