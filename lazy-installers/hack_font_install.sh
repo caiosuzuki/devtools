52ea2812332e72394b421e3577b5e9312de145db
caiosuzuki <cpolidoro@gmail.com>
2019-09-26 08:12:30 -0400

    adding hack font installer

---
+++ lazy-installers/hack_font_install.sh
@@ -0,0 +1,5 @@
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.2.0/Hack.zip -P $HOME/Downloads &&
+	mkdir $HOME/.fonts &&
+	unzip $HOME/Downloads/Hack.zip -d $HOME/.fonts &&
+	rm $HOME/Downloads/Hack.zip
+
