4546cc847dcbf95d2d4214d21213625144d4757f
caiosuzuki <cpolidoro@gmail.com>
2020-03-18 00:49:56 -0400

    including .deb dependency installers for sql server on ubuntu 19.04

--- lazy-installers/sql_server_install.sh
+++ lazy-installers/sql_server_install.sh
@@ -1,3 +1,11 @@
+echo "Default setup for Ubuntu 16.04"
+
+# For Ubuntu 19.04:
+# wget http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u12_amd64.deb &&
+# wget http://ftp.osuosl.org/pub/ubuntu/pool/universe/j/jemalloc/libjemalloc1_3.6.0-11_amd64.deb &&
+# sudo dpkg -i libssl1.0.0_1.0.1t-1+deb8u11_amd64.deb &&
+# sudo dpkg -i libjemalloc1_3.6.0-11_amd64.deb &&
+
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - \
 	&& sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2017.list)" \
 	&& sudo apt update -y \
