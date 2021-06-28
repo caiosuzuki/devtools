35c8b8319ad27dd854c3e70fa9ca5389c46dbca3
caiosuzuki <cpolidoro@gmail.com>
2021-06-28 08:25:11 -0400

    changing restore script to use docker shell script, removing old sql scripts to restore database

--- baks/sqls/restore_cheff.sql
+++
@@ -1,5 +0,0 @@
-USE [master]
-ALTER DATABASE [CheffEscolar] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
-BACKUP LOG [CheffEscolar] TO  DISK = N'/var/opt/mssql/data/CheffEscolar_LogBackup_2019-07-04_11-06-36.bak' WITH NOFORMAT, NOINIT,  NAME = N'CheffEscolar_LogBackup_2019-07-04_11-06-36', NOSKIP, NOREWIND, NOUNLOAD,  NORECOVERY ,  STATS = 5
-RESTORE DATABASE [CheffEscolar] FROM  DISK = N'/home/caio/devtools/baks/cheffEscolarModelo.bak' WITH  FILE = 1,  MOVE N'CheffEscolar' TO N'/var/opt/mssql/data/CheffEscolar.mdf',  MOVE N'CheffEscolar_log' TO N'/var/opt/mssql/data/CheffEscolar_log.ldf',  NOUNLOAD,  REPLACE,  STATS = 5
-ALTER DATABASE [CheffEscolar] SET MULTI_USER
